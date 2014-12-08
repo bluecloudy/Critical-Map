4734fba29fdfd014f30a8007d4e030e680694469
redone80 <dh.hoat@gmail.com>
2014-12-08 10:25:40 +0700

    color level

--- src/components/list-locations/list-locations.js
+++ src/components/list-locations/list-locations.js
@@ -48,6 +48,10 @@ define(['knockout', 'text!./list-locations.html', 'core'], function (ko, templat
             // Panto marker
             se.sandbox.publish('map:setCenter', item.position, this);
         };
+        self.onClose = function()
+        {
+            self.viewdetail(false);
+        }
 
         se.sandbox.publish('map:event:on', 'click', function () {
             self.selectedItem(null);
